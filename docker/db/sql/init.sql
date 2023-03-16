f61f70f5f0ab1d3f527c87af7f86740d495ab09b
tahara-h <hiroyasu.tahara@epkotsoftware.co.jp>
2023-03-16 15:59:06 +0900

    すべて削除

--- docker/db/sql/init.sql
+++
@@ -1,4 +0,0 @@
--- DB生成「laravel」
-CREATE DATABASE `laravel`;
--- DB生成「laravel_test」
-CREATE DATABASE `laravel_test`;
--- root/resources/views/top/user_top.blade.php
+++
@@ -1,8 +0,0 @@
-@extends("layout")
-@section('title','勤怠管理システム')
-@section('left_menu')
-    @include('user_left_menu')
-@endsection
-@section('contents')
-    @include('user_contents')
-@endsection
