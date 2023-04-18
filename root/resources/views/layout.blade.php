1474e52fe9535a5826757325a113e5714b6b075b
tahara-h <hiroyasu.tahara@epkotsoftware.co.jp>
2023-04-19 00:49:55 +0900

    jQueryの実装

--- root/resources/views/layout.blade.php
+++ root/resources/views/layout.blade.php
@@ -8,6 +8,8 @@
     <!-- bootstrapを読み込むためのコード -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="{{ asset('css/style.css')}}" rel="stylesheet">
+    <script src="{{ asset('js\jquery-3.6.4.min.js')}}"></script>
+    <script src="{{ asset('js/index.js')}}"></script>
 </head>
 
 <body>
